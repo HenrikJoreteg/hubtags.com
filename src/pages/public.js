954508283e75f2d40fbc5243fdf1237f25131436
Henrik Joreteg <hjoreteg@gmail.com>
2015-04-03 08:58:33 -0700

    minor style tweaks

--- src/pages/public.js
+++ src/pages/public.js
@@ -13,10 +13,10 @@ export default React.createClass({
       <div className='container'>
         <header role='banner'>
           <h1>HubTags</h1>
-          <p>Open source demo app, built with Ampersand.js and React by <a href='http://twitter.com/henrikjoreteg'>@HenrikJoreteg</a></p>
-          <p>Source <a href='https://github.com/henrikjoreteg/hubtags.com'>available on GitHub</a>.</p>
         </header>
         <div>
+          <p>Open source app for managing your tags (labels) on GitHub.com. Built with Ampersand.js and React by <a href='http://twitter.com/henrikjoreteg'>@HenrikJoreteg</a></p>
+          <p>Source <a href='https://github.com/henrikjoreteg/hubtags.com'>available on GitHub</a>.</p>
           <a href='/login' onClick={this.onLoginClick} className='button button-large'>
             <span className='mega-octicon octicon-mark-github'></span> Login with GitHub
           </a>
