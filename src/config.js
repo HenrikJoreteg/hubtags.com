201b3d72473ddd88fcb54adaa360d306012e1e04
Henrik Joreteg <hjoreteg@gmail.com>
2015-04-06 16:23:38 -0700

    fix github auth scope request

--- src/config.js
+++ src/config.js
@@ -5,20 +5,20 @@ const origin = window.location.origin
 let config = {
   isDev: hostname === 'localhost',
   apiUrl: 'https://api.github.com',
-  scopes: 'user,repo'
+  scope: 'user,repo'
 }
 
 if (config.isDev) {
   config.tokenUrl = 'https://hubtags-dev.herokuapp.com/authenticate'
   config.githubAuthUrl = 'https://github.com/login/oauth/authorize?' + qs.stringify({
-    scopes: config.scopes,
+    scope: config.scope,
     redirect_uri: origin + '/auth/callback',
     client_id: '34d32bcd940626d0d6f3'
   })
 } else {
   config.tokenUrl = 'https://hubtags.herokuapp.com/authenticate'
   config.githubAuthUrl = 'https://github.com/login/oauth/authorize?' + qs.stringify({
-    scopes: config.scopes,
+    scope: config.scope,
     redirect_uri: origin + '/auth/callback',
     client_id: 'a921b2a8818253f83ec7'
   })
