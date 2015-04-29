d2a09c0ad159664165d70b5c26764b3f0bde49cd
Henrik Joreteg <hjoreteg@gmail.com>
2015-04-28 21:06:49 -0700

    updating hjs-webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,26 +14,11 @@ module.exports = webpackConfig({
     config: 'src/config.js'
   },
   html: function (data) {
+    var layoutHtml = React.renderToString(React.createElement(Layout, {me: {}}))
+    var publicHtml = React.renderToString(React.createElement(PublicPage))
     return {
-      '200.html': template(data, React.renderToString(React.createElement(Layout, {me: {}}))),
-      'index.html': template(data, React.renderToString(React.createElement(PublicPage)))
+      '200.html': data.defaultTemplate({html: layoutHtml}),
+      'index.html': data.defaultTemplate({html: publicHtml})
     }
   }
 })
-
-function template (buildData, html) {
-  return [
-    '<!doctype>',
-    '<html>',
-      '<head>',
-        '<meta charset="utf-8"/>',
-        '<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">',
-        '<link rel="stylesheet" href="/' + buildData.css + '"/>',
-      '</head>',
-      '<body>',
-        html,
-      '</body>',
-      '<script src="/' + buildData.main + '"></script>',
-    '</html>'
-  ].join('')
-}
