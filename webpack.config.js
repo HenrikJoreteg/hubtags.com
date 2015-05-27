baefb156a2f8b6c78f0eac52ec909e3a9b4620d7
Henrik Joreteg <hjoreteg@gmail.com>
2015-05-26 21:10:30 -0700

    update hjs-webpack and simplify config accordingly

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,10 @@ var React = require('react')
 module.exports = webpackConfig({
   in: 'src/app.js',
   out: 'public',
-  isDev: env !== 'production',
   replace: {
     config: 'src/config.js'
   },
+  clearBeforeBuild: true,
   html: function (data) {
     var layoutHtml = React.renderToString(React.createElement(Layout, {me: {}}))
     var publicHtml = React.renderToString(React.createElement(PublicPage))
